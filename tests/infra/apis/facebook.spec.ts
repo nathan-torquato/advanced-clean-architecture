ed89345f31b6db85999b18b9f5cad5461d5e4228
Nathan Torquato <ntorquato@voice.com>
2021-07-22 09:22:25 -0300

    feat: ensure FacebookApi calls loadUser with correct params

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -2,37 +2,55 @@ import { LoadFacebookUserApi } from '@/data/contracts/apis'
 import { mock } from 'jest-mock-extended'
 
 export interface HttpClient {
-  get: <T = any>(params: HttpClient.Params) => Promise<T>
+  get: <T = any, U = any>(params: HttpClient.Params<U>) => Promise<T>
 }
 
 export namespace HttpClient {
-  export type Params = {
+  export type Params<U> = {
     url: string
+    params?: U
   }
 }
 
 export class FacebookAPI {
   private readonly baseUrl = 'https://graph.facebook.com/'
 
-  constructor (private readonly httpClient: HttpClient) {}
+  constructor (
+    private readonly httpClient: HttpClient,
+    private readonly clientId: string,
+    private readonly clientSecret: string
+  ) {}
 
   async loadUser (params: LoadFacebookUserApi.Params): Promise<void> {
     await this.httpClient.get({
-      url: `${this.baseUrl}oauth/access_token`
+      url: `${this.baseUrl}oauth/access_token`,
+      params: {
+        client_id: this.clientId,
+        client_secret: this.clientSecret,
+        grant_type: 'client_credentials'
+      }
     })
   }
 }
 
 describe('FacebookAPI', () => {
   const baseUrl = 'https://graph.facebook.com/'
+  const clientId = 'any_client_id'
+  const clientSecret = 'any_client_secret'
 
-  it('should loadUser to get app token', async () => {
+  it('should call loadUser with correct params', async () => {
     const httpClient = mock<HttpClient>()
-    const sut = new FacebookAPI(httpClient)
+    const sut = new FacebookAPI(httpClient, clientId, clientSecret)
     await sut.loadUser({ token: 'any_cient_token' })
-    expect(httpClient.get).toHaveBeenCalledTimes(1)
+
     expect(httpClient.get).toHaveBeenCalledWith({
-      url: `${baseUrl}oauth/access_token`
+      url: `${baseUrl}oauth/access_token`,
+      params: {
+        client_id: clientId,
+        client_secret: clientSecret,
+        grant_type: 'client_credentials'
+      }
     })
+    expect(httpClient.get).toHaveBeenCalledTimes(1)
   })
 })
