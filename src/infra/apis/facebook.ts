602193f84a60c7d11643d3f754ead13466dd4160
Nathan Torquato <ntorquato@voice.com>
2021-07-22 10:08:15 -0300

    refactor: improve code quality

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -1,6 +1,22 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
 import { HttpGetClient } from '@/infra/http'
 
+type AppToken = {
+  access_token: string
+}
+
+type DebugToken = {
+  data: {
+    user_id: string
+  }
+}
+
+type UserInfo = {
+  id: string
+  name: string
+  email: string
+}
+
 export class FacebookAPI implements LoadFacebookUserApi {
   private readonly baseUrl = 'https://graph.facebook.com'
 
@@ -11,7 +27,16 @@ export class FacebookAPI implements LoadFacebookUserApi {
   ) {}
 
   async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
-    const appToken = await this.httpClient.get({
+    const userInfo = await this.getUserInfo(params)
+    return {
+      facebookId: userInfo.id,
+      name: userInfo.name,
+      email: userInfo.email
+    }
+  }
+
+  private async getAppToken (): Promise<AppToken> {
+    return await this.httpClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
       params: {
         client_id: this.clientId,
@@ -19,16 +44,22 @@ export class FacebookAPI implements LoadFacebookUserApi {
         grant_type: 'client_credentials'
       }
     })
+  }
 
-    const debugToken = await this.httpClient.get({
+  private async getDebugToken (params: LoadFacebookUserApi.Params): Promise<DebugToken> {
+    const appToken = await this.getAppToken()
+    return await this.httpClient.get({
       url: `${this.baseUrl}/debug_token`,
       params: {
         access_token: appToken.access_token,
         input_token: params.token
       }
     })
+  }
 
-    const fbuser = await this.httpClient.get({
+  private async getUserInfo (params: LoadFacebookUserApi.Params): Promise<UserInfo> {
+    const debugToken = await this.getDebugToken(params)
+    return await this.httpClient.get({
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
       url: `${this.baseUrl}/${debugToken.data.user_id}`,
       params: {
@@ -36,11 +67,5 @@ export class FacebookAPI implements LoadFacebookUserApi {
         access_token: params.token
       }
     })
-
-    return {
-      facebookId: fbuser.id,
-      email: fbuser.email,
-      name: fbuser.name,
-    }
   }
 }
