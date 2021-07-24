d7e037fde3e1c1ba4c4003fb1045cfcc7975efcb
Nathan Torquato <ntorquato@voice.com>
2021-07-23 21:01:24 -0300

    refactor: move JwtTokenGenerator to its own file

---
+++ src/infra/crypto/jwt-token-generator.ts
@@ -0,0 +1,15 @@
+import { TokenGenerator } from '@/data/contracts/crypto'
+
+import jwt from 'jsonwebtoken'
+
+export class JwtTokenGenerator implements TokenGenerator {
+  constructor (private readonly secret: string) {}
+
+  async generateToken (params: TokenGenerator.Params): Promise<TokenGenerator.Result> {
+    const expiryInSeconds = params.expirationInMs / 1000
+
+    return jwt.sign({ key: params.key }, this.secret, {
+      expiresIn: expiryInSeconds
+    })
+  }
+}
